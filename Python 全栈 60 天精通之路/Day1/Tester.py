927e1e3d5bae93d64308a6c07a9e6ec1cc3ce71c
aiyuechuang <1432803776@qq.com>
2021-04-20 20:57:36 +0800

    update

--- Python 全栈 60 天精通之路/Day1/Tester.py
+++ Python 全栈 60 天精通之路/Day1/Tester.py
@@ -5,3 +5,22 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+
+
+class Vector:
+	def __init__(self, a, b):
+		self.a = a
+		self.b = b
+	
+	def __str__(self):
+		return 'Vector (%d, %d)' % (self.a, self.b)
+	
+	def __add__(self, other):
+		return Vector(self.a + other.a, self.b + other.b)
+
+
+v1 = Vector(2, 10)
+v2 = Vector(5, -2)
+v3 = Vector(10, 10)
+print(v1 + v2 + v3)
+print(v1)
