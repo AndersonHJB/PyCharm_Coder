5eb37e9371bb3522567a92ab552c5aec2d61ee45
aiyuechuang <1432803776@qq.com>
2021-04-20 19:37:39 +0800

    update

--- Python 全栈 60 天精通之路/Day1/class_Book.py
+++ Python 全栈 60 天精通之路/Day1/class_Book.py
@@ -5,3 +5,21 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+class Book(object):
+	# 定义类的参数
+	def __init__(self, book_id, book_name, book_store_count):
+		self.book_id = book_id
+		self.book_name = book_name
+		self.book_store_count = book_store_count
+	
+	# 重写加法操作
+	def __add__(self, book):
+		return self.book_store_count + book.book_store_count
+
+
+# 创建两个 Book 类的实例：
+python_intro_book = Book(1, 'python入门书', 100)
+ml_intro_book = Book(2, '机器学习入门书', 200)
+# 求两本书的总销量
+sales_cnt = python_intro_book + ml_intro_book
+print(sales_cnt)  # 300
