1a09038fc2a0183761ed21f7c422d061b3c170a0
aiyuechuang <1432803776@qq.com>
2021-04-21 09:53:08 +0800

    update

---
+++ Coder_Old/快捷启动/停止/stop_1.py
@@ -0,0 +1,20 @@
+import os
+
+#终止QQ软件
+def stopapp():
+	try:
+		os.system("taskkill /F /IM QQ.exe")
+		os.system("taskkill /F /IM pycharm64.exe")
+		os.system("taskkill /F /IM YoudaoDict.exe")
+		os.system("taskkill /F /IM WeChat.exe")
+		os.system("taskkill /F /IM sublime_text.exe")
+		# os.system("taskkill /F /IM chrome.exe")
+		os.system("taskkill /F /IM RuijieSupplicant.exe")
+		os.system("taskkill /F /IM PanDownload.exe")
+		os.system("taskkill /F /IM baidunetdisk.exe")
+		os.system("taskkill /F /IM ThunderStart.exe")
+	except:
+		print("关闭错误！")
+stopapp()
+#......
+#当然你还可以添加很多你需要终止的软件
---
+++ Coder_Old/快捷启动/启动/main_1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Author     ：AI悦创
+# @DateTime   ：2019/9/12  21:20 
+# @FileName   ：main_1.PY
+# @Function   ：功能
+# Development_tool   ：PyCharm
+# <-------import data-------------->
+import win32api
+
+def runapp():
+	try:
+		win32api.ShellExecute(0, 'open', r'D:\文字课程', '','',1)
+		win32api.ShellExecute(0, 'open', r'D:\文字课程\爬虫专题', '','',1)
+		win32api.ShellExecute(0, 'open', r'C:\Program Files\Sublime Text 3\sublime_text.exe', '','',1)
+		win32api.ShellExecute(0, 'open', r'D:\App\PyCharm 2019.2.2\bin\pycharm64.exe', '','',1)
+		win32api.ShellExecute(0, 'open', r'C:\Program Files (x86)\Youdao\Dict\YoudaoDict.exe', '','',1)
+		win32api.ShellExecute(0, 'open', r'D:\App\Tencent\QQ\Bin\QQScLauncher.exe', '','',1)
+		win32api.ShellExecute(0, 'open', r'D:\App\Tencent\WeChat\WeChat.exe', '','',1)
+	except:
+		print('错误！')
+runapp()
+# win32api.ShellExecute(0, 'open', r'你的应用程序路径', '','',1)
+
+
