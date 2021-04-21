27e50d045f4edd326d62729b29ad1497a0a16ce4
aiyuechuang <1432803776@qq.com>
2021-04-21 10:54:59 +0800

    update

--- Student_Coder/Austin_Coder/20210421/lesson01.py
+++ Student_Coder/Austin_Coder/20210421/lesson01.py
@@ -9,15 +9,19 @@
 # print(help("keywords"))
 
 # 变量赋值
+# 布尔型
 bool1 = True
 bool2 = False
 
+# 字符串
 str1 = 'Hello, Austin!'
 str2 = "Hello, Austin!"
 str3 = '''Hello, Austin!'''
 
+# 元组
 tup = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
 
+# 列表
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
 # command + option + L：代码美化/格式化
