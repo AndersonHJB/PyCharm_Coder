0437f7153c65dbdd4468a527c3f1bb6bbf637b2c
aiyuechuang <1432803776@qq.com>
2021-04-22 22:38:47 +0800

    update

---
+++ Python 全栈 60 天精通之路/Day2/str_translate.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/22 5:59 下午
+# @Author  : AI悦创
+# @FileName: str_translate.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+def is_rotation(s1: str, s2: str) -> bool:
+	if s1 is None or s2 is None:
+		return False
+	if len(s1) != len(s2):
+		return False
+	
+	def is_substring(s1: str, s2: str) -> bool:
+		return s1 in s2
+	
+	return is_substring(s1, s2 + s2)
