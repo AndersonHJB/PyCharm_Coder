decbd563e27883c6fe6d8e58e2696de9ea96bc92
aiyuechuang <1432803776@qq.com>
2021-04-23 10:39:46 +0800

    update

---
+++ Student_Coder/Austin_Coder/20210423/lesson03.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/23 10:39 上午
+# @Author  : AI悦创
+# @FileName: lesson03.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+每秒花费 = 100
+秒数 = 20
+单位值 = 60
+print()
