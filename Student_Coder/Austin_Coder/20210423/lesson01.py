0094fbadd8386a7c7bcd92bc43f211aeaa20ae84
aiyuechuang <1432803776@qq.com>
2021-04-23 10:13:31 +0800

    update

---
+++ Student_Coder/Austin_Coder/20210423/lesson01.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/23 10:13 上午
+# @Author  : AI悦创
+# @FileName: lesson01.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
--- The_60_day_path_to_mastery_of_Python_full_stack/Day3/tuple_coder.py
+++ The_60_day_path_to_mastery_of_Python_full_stack/Day3/tuple_coder.py
@@ -8,3 +8,14 @@
 a = ()  # 空元组对象
 b = (1, 'xiaoming', 29.5, '17312662388')
 c = ('001', '2019-11-11', ['三文鱼', '电烤箱'])
+
+tup = (10)
+print(type(tup))  # <class 'int'>
+tup2 = (10,)
+print(type(tup2))  # <class 'tuple'>
+
+from numpy import random
+
+a = random.randint(1, 5, 10)  # 从 [1,5) 区间内随机选择 10 个数
+at = tuple(a)  # 转 tuple：(1, 4, 2, 1, 3, 3, 2, 3, 4, 2)
+at.count(3)  # 统计 3 出现次数，恰好也为 3 次
