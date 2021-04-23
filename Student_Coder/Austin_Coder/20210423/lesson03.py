f02a751984cb90a157dcb8bd2b713c8aaf0a6604
aiyuechuang <1432803776@qq.com>
2021-04-23 10:48:51 +0800

    update

--- Student_Coder/Austin_Coder/20210423/lesson03.py
+++ Student_Coder/Austin_Coder/20210423/lesson03.py
@@ -5,7 +5,7 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
-Spend_every_second = 100
-s = 20
-Unit_value = 60
+# 同时给多个变量赋相同值
+# command + option + L
+Spend_every_second, s, Unit_value = 100, 20, 60
 print(Spend_every_second * s * Unit_value)
