256406a9b49d9f3e6ad32833030ebfd06dbd60d2
aiyuechuang <1432803776@qq.com>
2021-04-23 10:27:04 +0800

    update

--- Student_Coder/Austin_Coder/20210423/lesson01.py
+++ Student_Coder/Austin_Coder/20210423/lesson01.py
@@ -5,3 +5,15 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+# 变量 = 表达式
+# 钱：几块钱 => 数字型 => int
+# ---- 第一步 ----
+sum_money = 30
+drink = 4
+lunch = 24
+
+# ---- 第二步 ----
+# result_money = sum_money - drink - lunch
+result_money = sum_money - (drink + lunch)
+print(result_money)
+
