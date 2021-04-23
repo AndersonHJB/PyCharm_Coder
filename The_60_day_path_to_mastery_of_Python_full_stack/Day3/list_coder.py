92db6c2988b6a40b3d9993f4da12aa3f62b8197e
aiyuechuang <1432803776@qq.com>
2021-04-23 09:49:15 +0800

    update

--- The_60_day_path_to_mastery_of_Python_full_stack/Day3/list_coder.py
+++ The_60_day_path_to_mastery_of_Python_full_stack/Day3/list_coder.py
@@ -42,4 +42,14 @@ a = [1, 2, [3, 4, 5]]
 ac = a.copy()
 ac[0] = 10  # [10, 2, [3, 4, 5]]
 ac[2][1] = 40  # [10, 2, [3, 40, 5]]
+print(a[0] == ac[0])  # False
+print(a[2][1] == ac[2][1])  # True
+
+from copy import deepcopy
+
+a = [1,2,[3,4,5]]
+ac = deepcopy(a)
+ac[0] = 10
+ac[2][1] = 40
 print(a[0] == ac[0])
+print(a[2][1] == ac[2][1])
