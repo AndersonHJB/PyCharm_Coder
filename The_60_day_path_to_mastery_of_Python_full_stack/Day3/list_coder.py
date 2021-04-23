149329c97daa42783c6851849435bc1be7384752
aiyuechuang <1432803776@qq.com>
2021-04-23 08:56:51 +0800

    update

--- The_60_day_path_to_mastery_of_Python_full_stack/Day3/list_coder.py
+++ The_60_day_path_to_mastery_of_Python_full_stack/Day3/list_coder.py
@@ -5,3 +5,32 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+empty = []
+lst = [1, 'xiaoming', 29.5, '17312662388']
+lst2 = ['001', '2019-11-11', ['三文鱼', '电烤箱']]
+print(len(empty))  # 0
+print(len(lst))  # 4
+print(len(lst2))  # 3
+
+for _ in lst:
+	print(f'{_}的类型为{type(_)}')
+
+sku = lst2[2]  # sku 又是一个列表
+
+sku.append('烤鸭')
+print(sku)  # ['三文鱼', '电烤箱', '烤鸭']
+
+sku.insert(1, '牛腱子')
+print(sku)  # ['三文鱼', '牛腱子', '电烤箱', '烤鸭']
+
+item = sku.pop()  # 返回烤鸭
+print(sku)  # ['三文鱼', '牛腱子', '电烤箱']
+
+sku.remove('三文鱼')  # 更好用：sku.remove(sku[0])
+print(sku)  # ['牛腱子', '电烤箱']
+
+print(lst2)  # ['001', '2019-11-11', ['牛腱子', '电烤箱']]
+
+lst2 = ['001', '2019-11-11', ['三文鱼', '电烤箱']]  # 这是lst2的初始值
+
+sku_deep = lst2[2].copy()
