993674c429fa94f7f841ab3f3f14b64b32ef17cc
aiyuechuang <1432803776@qq.com>
2021-04-24 11:10:59 +0800

    update

---
+++ Student_Coder/Austin_Coder/20210424/b_tuige.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/24 11:10 上午
+# @Author  : AI悦创
+# @FileName: b_tuige.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+str1 = "aiyc,austin"
+print(str1)
