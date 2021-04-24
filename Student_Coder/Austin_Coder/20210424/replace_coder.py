825eb309064b79faea947a2ade5d1324d80d0fe3
aiyuechuang <1432803776@qq.com>
2021-04-24 10:59:43 +0800

    update

--- Student_Coder/Austin_Coder/20210424/replace_coder.py
+++ Student_Coder/Austin_Coder/20210424/replace_coder.py
@@ -5,3 +5,7 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+# Pycharm 代码运行快捷键：control + shift + R
+str_name = "I am Lilei.Lilei,Lilei"
+new_name = str_name.replace("Lilei", "Austin", 3)
+print(new_name)
