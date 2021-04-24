1cb6c68ed1360d257e073e90fec7773a64e10f5f
aiyuechuang <1432803776@qq.com>
2021-04-24 10:55:36 +0800

    update

--- Student_Coder/Austin_Coder/20210424/lesson03.py
+++ Student_Coder/Austin_Coder/20210424/lesson03.py
@@ -5,3 +5,13 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+str_text = "aiyuecshuangs-Austin"
+# s 的下标
+# rfind = right find
+num = str_text.rfind("s")
+print(str_text.rfind("s"))
+print(str_text[num])
+# str1 = "AIYC"
+# str2 = "aiycA"
+# print(str1.lower())
+# print(str2.upper())
