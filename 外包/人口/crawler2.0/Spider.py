e054efa1095a545fcd57787fb8fbd231ef4f234d
aiyuechuang <1432803776@qq.com>
2021-04-25 22:16:32 +0800

    update

--- 外包/人口/crawler2.0/Spider.py
+++ 外包/人口/crawler2.0/Spider.py
@@ -43,15 +43,15 @@ def downloader(url, timeout=10, headers=None, debug=False, binary=False):
 	return status, html, redirected_url
 
 
-def write_csv(data):
-	with open("stocks.csv", "a+")as f:
-		f_csv = csv.writer(f)
-		f_csv.writerow(data)
+# def write_csv(data):
+# 	with open("stocks.csv", "a+")as f:
+# 		f_csv = csv.writer(f)
+# 		f_csv.writerow(data)
 
 
 def write_dict_csv(data):
-	headers = ['ranking', 'region', 'permanent_resident_population', 'area']
-	with open("stocks.csv", "a+")as f:
+	headers = ['ranking', 'region', 'permanent_resident_population', 'area', 'link']
+	with open("stocks.csv", "a+", encoding='gbk')as f:
 		f_csv = csv.DictWriter(f, headers)
 		f_csv.writeheader()
 		f_csv.writerows(data)
@@ -76,13 +76,15 @@ def parse(html):
 		region = data.select(".even .column-2")
 		permanent_resident_population = data.select(".even .column-3")
 		area = data.select(".even .column-4")
+		# link = data.select(".even .column-2 a").a.get('href')
 		# print("index:>>>", index)
 		if ranking and region and permanent_resident_population and area:
 			yield {
 				"ranking": ranking[0].string,
 				"region": region[0].string,
 				"permanent_resident_population": permanent_resident_population[0].string,
-				"area": area[0].string
+				"area": area[0].string,
+				# "link": link
 			}
 
 
@@ -91,6 +93,7 @@ if __name__ == '__main__':
 	s, html, lost_url = downloader(url)
 	# print(s, html, lost_url)
 	# print(list(parse(html)))
-	for item in parse(html):
-		print(item)
-		write_dict_csv(list(item))
+	data = list(parse(html))
+	# for item in parse(html):
+	# 	print(item)
+	write_dict_csv(data)
