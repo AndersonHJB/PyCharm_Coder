88a55397ca81e8ddd3cca3982e7ee6b654c23f6a
aiyuechuang <1432803776@qq.com>
2021-04-26 08:45:13 +0800

    update

---
+++ Personal_Practice_Code/20210426/lesson.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/26 8:28 上午
+# @Author  : AI悦创
+# @FileName: lesson.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+dict([('a', 1), ('b', 2)], c=3)
+
+{}.fromkeys(['k1', 'k2', 'k3'], [1, 2, 3])
+
+{'a': 1, 'b': 2}.fromkeys(['c', 'd'], [1, 2])
+
+d = {'a': 1, 'b': 2, 'c': 3}
+
+d = {'a': 1, 'b': 2, 'c': 3}
+
