1f4a58e8a7d102a42e233ca40a77b4180cbbe782
aiyuechuang <1432803776@qq.com>
2021-04-27 20:55:51 +0800

    update

--- Personal_Practice_Code/2021/202010427/spider.py
+++ Personal_Practice_Code/2021/202010427/spider.py
@@ -5,3 +5,12 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+import requests
+
+api = "https://music.163.com/weapi/comment/resource/comments/get?csrf_token="
+headers = {
+	"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36",
+}
+session = requests.session()
+session.headers = headers
+response = session.post()
