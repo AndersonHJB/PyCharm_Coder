17210b2a4b50af8fc24feca4a8cb7b7234023577
aiyuechuang <1432803776@qq.com>
2021-04-27 08:12:41 +0800

    update

--- Personal_Practice_Code/20210426/lesson.py
+++ Personal_Practice_Code/20210426/lesson.py
@@ -6,5 +6,11 @@
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
 
-dict1 = {"name": "Aiyuechuang", "age": 18}
-print(dict1)
+import requests
+
+url = "https://sh.58.com/shangpucz/?spm=u-2d2yxv86y3v43nkddh1.BDPCPZ_BT&gposLastIndex=179&utm_source=market&PGTID=0d306b31-0000-06bc-e453-46bb41f0b8b1&ClickID=2"
+headers = {
+		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36",
+	}
+r = requests.get(url, headers=headers)
+print(r.status_code)
