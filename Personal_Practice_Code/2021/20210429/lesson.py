b87886517b620e7d6d1384516fa011ee42b8f25b
aiyuechuang <1432803776@qq.com>
2021-04-29 15:27:42 +0800

    update

---
+++ Personal_Practice_Code/2021/20210429/lesson.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/29 3:26 下午
+# @Author  : AI悦创
+# @FileName: lesson.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+num1 = 10
+num2 = 15
+sum_num = num1 + num2
+print(sum_num)
