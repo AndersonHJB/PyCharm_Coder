659ed350bae697be1f92304e60cca2b11158843d
aiyuechuang <1432803776@qq.com>
2021-05-01 15:19:45 +0800

    update

--- Student_Coder/Austin_Coder/20210501/lesson.py
+++ Student_Coder/Austin_Coder/20210501/lesson.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2021/4/29 3:26 下午
+# @Time    : 2021/5/1 9:35 上午
 # @Author  : AI悦创
 # @FileName: lesson.py
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
-# 3. 直接删掉
