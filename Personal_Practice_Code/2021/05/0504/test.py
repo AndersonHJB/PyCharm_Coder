2257bf889e674b14e1010dce0ad669ab85555447
aiyuechuang <1432803776@qq.com>
2021-05-04 10:23:23 +0800

    update

---
+++ Personal_Practice_Code/2021/05/0504/test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/4 10:20 上午
+# @Author  : AI悦创
+# @FileName: test.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+print("Hello AI悦创。")
+print("")
