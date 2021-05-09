c4f9e6593db6e0d348625000b7ce5eb78f48e24d
aiyuechuang <1432803776@qq.com>
2021-05-09 20:12:34 +0800

    update

---
+++ 网易云课堂/Python办公自动化实战/02-环境搭建与快速入门/dict_code.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/9 8:10 下午
+# @Author  : AI悦创
+# @FileName: dict_code.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+dict_3 = {'姓名': 'mike', '年龄': 20}
+
+dict_4 = dict({'姓名': 'mike', '年龄': 20})
+
+dict_5 = dict(姓名='mike', 年龄=20)
