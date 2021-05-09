db066fcdc7ab8785e78971b14e1b1ffef5ab2ef7
aiyuechuang <1432803776@qq.com>
2021-05-09 19:06:52 +0800

    update

--- Personal_Practice_Code/2021/05/0509/lesson01.py
+++ Personal_Practice_Code/2021/05/0509/lesson01.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2021/5/9 8:57 上午
-# @Author  : AI悦创
-# @FileName: lesson01.py
-# @Software: PyCharm
-# @Blog    ：http://www.aiyc.top
-# @公众号   ：AI悦创
+personal_info = ['aiyc', '男']
 
+personal_info.append(1.74)
+
+personal_info.insert(1, 28)
+
+personal_info[1] = 36
+personal_info[0:2] = ['aiyc', 35]
+print(personal_info)
---
+++ 网易云课堂/Python办公自动化实战/02-环境搭建与快速入门/lst_code.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/9 7:05 下午
+# @Author  : AI悦创
+# @FileName: lst_code.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+personal_info = ['aiyc', '男']
+personal_info.append(1.74)
+personal_info.insert(1, 28)
+personal_info[1] = 36
+personal_info[0:2] = ['aiyc', 35]
+print(personal_info)
