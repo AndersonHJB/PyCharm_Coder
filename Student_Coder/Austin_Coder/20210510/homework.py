a80bb3ef4c119e7363d50dd06c4f41f5ca167f53
aiyuechuang <1432803776@qq.com>
2021-05-10 11:13:21 +0800

    update

--- Student_Coder/Austin_Coder/20210510/homework.py
+++ Student_Coder/Austin_Coder/20210510/homework.py
@@ -5,16 +5,16 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
-username = input("请输入您的姓名：")
-sex = input("请输入您的性别：")
-user_age = input("请输入您的年龄")
-
-print("正在生成您的简历......")
-print("****************")
-print("简历")
-print("姓名：", username)
-print("性别：" + sex)
-print("年龄：", user_age)
+# username = input("请输入您的姓名：")
+# sex = input("请输入您的性别：")
+# user_age = input("请输入您的年龄")
+#
+# print("正在生成您的简历......")
+# print("****************")
+# print("简历")
+# print("姓名：", username)
+# print("性别：" + sex)
+# print("年龄：", user_age)
 
 # input('''
 # Name：黄家宝
@@ -22,3 +22,16 @@ print("年龄：", user_age)
 # age：21
 # school：厦门工学院
 # ''')
+
+username = input("请输入您的姓名：")
+sex = input("请输入您的性别：")
+user_age = int(input("请输入您的年龄："))
+school = input("请输入您的学校：")
+
+print("正在生成您的简历......")
+print("*" * 12)
+print('\t\t简历')
+print("姓名：", username)
+print("性别：", sex)
+print("年龄：", user_age)
+print("学校：", school)
