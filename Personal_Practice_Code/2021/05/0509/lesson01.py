72515814e366ace4f1bb7999039393dac1fec04a
aiyuechuang <1432803776@qq.com>
2021-05-10 17:27:44 +0800

    update

--- Personal_Practice_Code/2021/05/0509/lesson01.py
+++ Personal_Practice_Code/2021/05/0509/lesson01.py
@@ -1,9 +1,3 @@
-personal_info = ['aiyc', '男']
-
-personal_info.append(1.74)
-
-personal_info.insert(1, 28)
-
-personal_info[1] = 36
-personal_info[0:2] = ['aiyc', 35]
-print(personal_info)
+import qrcode
+img = qrcode.make('0aaa你呢a1010')
+img.save('filename.png')
