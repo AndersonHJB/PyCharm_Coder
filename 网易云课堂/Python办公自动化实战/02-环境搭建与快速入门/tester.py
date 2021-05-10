1007e9af549a9ba8d6f72749ff716f768fc02a3f
aiyuechuang <1432803776@qq.com>
2021-05-10 09:11:08 +0800

    update

---
+++ 网易云课堂/Python办公自动化实战/02-环境搭建与快速入门/tester.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/9 9:01 下午
+# @Author  : AI悦创
+# @FileName: tester.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+dict_1 = {'name': 'mike', 'age': 28, 'city': 'Beijing'}
+dict_2 = {'names': ('mike', 'Tim'), 'cities': ['Beijing', 'SH']}
