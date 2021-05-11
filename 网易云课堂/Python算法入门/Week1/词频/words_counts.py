0e449702b6c1dc1422e7a3415645fe61f3c29c73
aiyuechuang <1432803776@qq.com>
2021-05-11 21:33:57 +0800

    update

--- 网易云课堂/Python算法入门/Week1/词频/words_counts.py
+++ 网易云课堂/Python算法入门/Week1/词频/words_counts.py
@@ -5,3 +5,44 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+word_list = []
+def words_count():
+	words_dict = {}
+	for word in word_list:
+		if word in words_dict:
+			words_dict[word] += 1
+		else:
+			words_dict[word] = 1
+	return words_dict
+
+
+def read_txt():
+	with open('I_Have_a_Dream.txt', mode='r', encoding='utf-8') as f:
+		"""
+		f.read() -> type: <class 'str'>
+		f.readline() -> type: <class 'str'> -> Read a line
+		f.readlines() -> type: <class 'list'> -> Read all
+		"""
+		lines = f.readlines()
+		for line in lines:
+			line = line.replace(',', '')
+			line = line.replace(':', '')
+			line = line.replace('?', '')
+			line = line.replace('!', '')
+			line = line.replace('"', '')
+			line = line.replace('\n', '')
+			line = line.replace('”', '')
+			line = line.replace('.', '')
+			line = line.replace(';', '')
+			line = line.replace('“', '')
+			for word in line.split(' '):
+				if word: word_list.append(word)
+
+
+def main():
+	read_txt()
+	print(words_count())
+
+
+if __name__ == '__main__':
+	main()
