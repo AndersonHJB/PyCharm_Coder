982c71dede41e54d63eb69404afbbfa51613492f
aiyuechuang <1432803776@qq.com>
2021-05-13 11:16:28 +0800

    update

---
+++ Personal_Practice_Code/2021/05/0511/lesson02.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/12 2:39 下午
+# @Author  : AI悦创
+# @FileName: lesson02.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+print('考试及格')
