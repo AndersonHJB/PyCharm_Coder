6772b8614582d66473069746bf761cb286c27988
aiyuechuang <1432803776@qq.com>
2021-05-14 14:24:38 +0800

    update

--- 网易云课堂/Python办公自动化实战/03-文件的操作/os_walk_learn.py
+++ 网易云课堂/Python办公自动化实战/03-文件的操作/os_walk_learn.py
@@ -5,17 +5,18 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+# Python os.walk() 笔记：https://www.aiyc.top/1672.html
 import os
 
-path = '.'
-# for i in os.walk(path):
-# 	print(i)
+path = 'Path_file'
+for i in os.walk(path):
+	print(i)
 # ("当前文件夹的地址", "当前文件夹下的文件夹名称", "当前文件夹下的文件，不包含子目录文件")
 
-# import os
-
-for root, dirs, files in os.walk(".", topdown=False):
-	for name in files:
-		print(os.path.join(root, name))
-	for name in dirs:
-		print(os.path.join(root, name))
+# topdown=True：先找当前目录下的
+# topdown=False：先找子目录下的
+# for root, dirs, files in os.walk(".", topdown=False):
+# 	for name in files:
+# 		print(os.path.join(root, name))
+# 	for name in dirs:
+# 		print(os.path.join(root, name))
