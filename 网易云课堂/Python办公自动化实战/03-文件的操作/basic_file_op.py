984b8ef0efea409ca61857a1840ef644f13014f5
aiyuechuang <1432803776@qq.com>
2021-05-14 14:50:44 +0800

    update

--- 网易云课堂/Python办公自动化实战/03-文件的操作/basic_file_op.py
+++ 网易云课堂/Python办公自动化实战/03-文件的操作/basic_file_op.py
@@ -2,15 +2,16 @@ import os
 from shutil import copy, move
 
 # 基础文件获取
-path = 'data_file'  # os.walk 返回 文件路径 文件夹 文件
+path = 'Path_file'  # os.walk 返回：文件路径 文件夹 文件
 
-for filepath, folders, files in os.walk(path):
-	# print(filepath,folders,files)
-	for file in files:
-		print(os.path.join(filepath, file))
-		# print(file)
+# for filepath, folders, files in os.walk(path):
+# 	# print(filepath, folders, files)
+# 	for file in files:
+# 		print(os.path.join(filepath, file))
+# 		# print(file)
 
-# for file in os.listdir(path):
-# 	# print(file)
-# 	if 'txt' in file:
-# 		print(file)
+# listdir:不提取子目录，只提取子目录的名称
+for file in os.listdir(path):
+	# print(file)
+	if 'txt' in file:
+		print(file)
---
+++ 网易云课堂/Python办公自动化实战/03-文件的操作/desktop_task-1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/14 2:50 下午
+# @Author  : AI悦创
+# @FileName: desktop_task-1.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
