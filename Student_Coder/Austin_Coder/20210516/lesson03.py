f1eda2e4042c8c1898aae9babd8e570d283b4b01
aiyuechuang <1432803776@qq.com>
2021-05-16 09:14:08 +0800

    update

---
+++ Student_Coder/Austin_Coder/20210516/lesson03.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/16 9:12 上午
+# @Author  : AI悦创
+# @FileName: lesson03.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+x = "Hello World!"
+new_x = x.replace("Hello", "Hi")
+print(new_x)
