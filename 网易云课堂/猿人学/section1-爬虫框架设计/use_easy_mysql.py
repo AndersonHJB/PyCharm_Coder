c9a8198b45ad601e84b213ddbceeb46866c06596
aiyuechuang <1432803776@qq.com>
2021-05-19 20:59:32 +0800

    update

--- 网易云课堂/猿人学/section1-爬虫框架设计/use_easy_mysql.py
+++ 网易云课堂/猿人学/section1-爬虫框架设计/use_easy_mysql.py
@@ -9,17 +9,17 @@ from easy_mysql import Connection
 
 db = Connection(
 	'localhost',
-	'db_name',
-	'user',
-	'password'
+	'easymysqldb',
+	'root',
+	'Cleland621700'
 )
-# 获取一条记录
-sql = 'select * from test_table where id=%s'
-data = db.get(sql, 2)
-
-# 获取多天记录
-sql = 'select * from test_table where id>%s'
-data = db.query(sql, 2)
+# # 获取一条记录
+# sql = 'select * from test_table where id=%s'
+# data = db.get(sql, 2)
+#
+# # 获取多天记录
+# sql = 'select * from test_table where id>%s'
+# data = db.query(sql, 2)
 
 # 插入一条数据
 sql = 'insert into test_table(title, url) values(%s, %s)'
