14af1a6b953db81a5694983544dc39956c691d8d
aiyuechuang <1432803776@qq.com>
2021-05-21 23:18:27 +0800

    update

--- Student_Coder/Go_your_own_way/variable/lesson01.py
+++ Student_Coder/Go_your_own_way/variable/lesson01.py
@@ -5,3 +5,28 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+x=1
+x=1
+x=1
+x=1
+x=1
+x=1
+x=1
+x=1
+x=1
+x=1
+x=1
+x = 1
+x = 1
+x = 1
+x = 1
+x = 1
+x = 1
+x = 1
+x = 1
+x = 1
+# 有没有代码洁癖：有，我写的代码符合 PEP8
+# utf-8
+x = x + 10
+print(x)
+# 来自高手之间的：不过如此
---
+++ Student_Coder/Go_your_own_way/variable/lesson02.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/21 11:16 下午
+# @Author  : AI悦创
+# @FileName: lesson02.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+name1 = 'lilei'
+name2 = name1
+print(name2)
