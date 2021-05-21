533da4afc34ae5328183c7e937feb8beef8d7bf7
aiyuechuang <1432803776@qq.com>
2021-05-21 15:39:19 +0800

    update

--- 网易云课堂/猿人学/section1-爬虫框架设计/study_urlparse_clean_source.py
+++ 网易云课堂/猿人学/section1-爬虫框架设计/study_urlparse_clean_source.py
@@ -5,3 +5,23 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+# 4. 去掉标识流量来源的参数
+    # badquery = ['spm', 'utm_source', 'utm_source', 'utm_medium', 'utm_campaign']
+    good_queries = []
+    for query in up.query.split('&'):
+        qv = query.split('=')
+        if qv[0].startswith('spm') or qv[0].startswith('utm_'):
+            continue
+        if len(qv) == 1:
+            continue
+        good_queries.append(query)
+    query = '&'.join(good_queries)
+    url = urlparse.urlunparse((
+        up.scheme,
+        up.netloc,
+        path,
+        up.params,
+        query,
+        ''  # crawler do not care fragment
+    ))
+    return url
