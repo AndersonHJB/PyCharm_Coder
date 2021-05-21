3cf546718dbf4f963c9d09edc6fd5a01a4d0880e
aiyuechuang <1432803776@qq.com>
2021-05-21 11:08:23 +0800

    update

--- 网易云课堂/猿人学/section1-爬虫框架设计/Tester_Coder/less.py
+++ 网易云课堂/猿人学/section1-爬虫框架设计/Tester_Coder/less.py
@@ -5,10 +5,22 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+from urllib.parse import urlparse
+
+g_bin_postfix = set([
+    'exe', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx',
+    'pdf',
+    'jpg', 'png', 'bmp', 'jpeg', 'gif',
+    'zip', 'rar', 'tar', 'bz2', '7z', 'gz',
+    'flv', 'mp4', 'avi', 'wmv', 'mkv',
+    'apk',
+])
+
+url = "http://app.cctv.com/special/cportal/detail/arti/index.html?id=ArtiLJYd8ExpiOe8H9ytXu8c210520&fromapp=cctvnews&version=809&allow_comment=1&allow_comment=1"
 up = urlparse.urlparse(url)
-    path = up.path
-    if not path:
-        path = '/'
-    postfix = path.split('.')[-1].lower()
-    if postfix in g_bin_postfix:
-        return ''
+path = up.path
+if not path:
+    path = '/'
+postfix = path.split('.')[-1].lower()
+if postfix in g_bin_postfix:
+    print("No")
