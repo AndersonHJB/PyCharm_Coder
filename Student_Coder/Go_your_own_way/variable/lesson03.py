406e34d0b1137c73d6ea36fce76cf8134d1acd0d
aiyuechuang <1432803776@qq.com>
2021-05-22 13:07:55 +0800

    update

--- Student_Coder/Go_your_own_way/variable/lesson03.py
+++ Student_Coder/Go_your_own_way/variable/lesson03.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2021/5/21 11:16 下午
+# @Time    : 2021/5/21 11:18 下午
 # @Author  : AI悦创
-# @FileName: lesson02.py
+# @FileName: lesson03.py
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
 name1 = 'lilei'
-name2 = name1
-print(name2)
+name1 = 'hanmeimei'
+# 字符串
+print("这是 name1 的值：", name1)
