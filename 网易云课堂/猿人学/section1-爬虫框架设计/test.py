8adba95fe46e858c07936d6809e099ff61216b63
aiyuechuang <1432803776@qq.com>
2021-05-24 17:42:14 +0800

    update

---
+++ 网易云课堂/猿人学/section1-爬虫框架设计/test.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/24 5:42 下午
+# @Author  : AI悦创
+# @FileName: test.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+#!/usr/bin/env python3
+# Author: veelion
+
+
+import re
+import time
+import requests
+import tldextract
+
+
+def save_to_db(url, html):
+    # 保存网页到数据库，我们暂时用打印相关信息代替
+    print('%s : %s' % (url, len(html)))
+
+
+def crawl():
+    # 1. download baidu news
+    hub_url = 'http://news.baidu.com/'
+    res = requests.get(hub_url)
+    html = res.text
+
+    # 2. extract news links
+    ## 2.1 extract all links with 'href'
+    links = re.findall(r'href=[\'"]?(.*?)[\'"\s]', html)
+    print('find links:', len(links))
+    news_links = []
+    ## 2.2 filter non-news link
+    for link in links:
+        if not link.startswith('http'):
+            continue
+        tld = tldextract.extract(link)
+        if tld.domain == 'baidu':
+            continue
+        news_links.append(link)
+
+    print('find news links:', len(news_links))
+    # 3. download news and save to database
+    for link in news_links:
+        html = requests.get(link).text
+        save_to_db(link, html)
+    print('works done!')
+
+
+def main():
+    while 1:
+        crawl()
+        time.sleep(300)
+
+
+if __name__ == '__main__':
+    main()
