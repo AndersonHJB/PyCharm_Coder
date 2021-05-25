2c30ed642495036d73225bde4656ff7420a66284
aiyuechuang <1432803776@qq.com>
2021-05-25 23:45:23 +0800

    update

---
+++ 网易云课堂/猿人学/资料/virtualappyrx/VirtualApp/app-arm64/src/main/java/io/virtualapp/addon/arm64/DebugActivity.java
@@ -0,0 +1,13 @@
+package io.virtualapp.addon.arm64;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DebugActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
