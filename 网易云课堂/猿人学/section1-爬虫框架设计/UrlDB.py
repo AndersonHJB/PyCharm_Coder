47b626a7731cbd2b5c54a519b8baaba325f3740e
aiyuechuang <1432803776@qq.com>
2021-05-26 11:14:32 +0800

    update

--- 网易云课堂/猿人学/section1-爬虫框架设计/UrlDB.py
+++ 网易云课堂/猿人学/section1-爬虫框架设计/UrlDB.py
@@ -9,52 +9,59 @@ from redis import Redis
 
 
 class UrlDB(object):
-    '''
-    Use redis to store URLs what have been done(succeed or faile)
-    '''
-    status_failure = b'0'
-    status_success = b'1'
+	'''
+	Use redis to store URLs what have been done(succeed or faile)
+	'''
+	status_failure = b'0'
+	status_success = b'1'
+	
+	def __init__(self, db_name):
+		self.name = db_name
+		self.db = Redis(host='localhost', port=6379, db=self.name)
+	
+	def set_success(self, url):
+		"""
+		请求成功的 url
+		"""
+		if isinstance(url, str):
+			url = url.encode('utf8')
+		try:
+			self.db.set(url, self.status_success)
+			s = True
+		except:
+			s = False
+		return s
+	
+	def set_failure(self, url):
+		"""
+		请求失败的 url
+		"""
+		if isinstance(url, str):
+			url = url.encode('utf8')
+		try:
+			self.db.set(url, self.status_failure)
+			s = True
+		except:
+			s = False
+		return s
+	
+	def has(self, url):
+		"""
+		判断我们的 url 是否存在
+		"""
+		if isinstance(url, str):
+			url = url.encode('utf8')
+		try:
+			# attr = self.db.get(url)
+			attr = self.db.exists(url)
+			return attr
+		except:
+			pass
+		return False
 
-    def __init__(self, db_name):
-        self.name = db_name
-        self.db = Redis(host='localhost', port=6379, db=self.name)
 
-    def set_success(self, url):
-        """
-        请求成功的 url
-        """
-        if isinstance(url, str):
-            url = url.encode('utf8')
-        try:
-            self.db.set(url, self.status_success)
-            s = True
-        except:
-            s = False
-        return s
-
-    def set_failure(self, url):
-        """
-        请求失败的 url
-        """
-        if isinstance(url, str):
-            url = url.encode('utf8')
-        try:
-            self.db.set(url, self.status_failure)
-            s = True
-        except:
-            s = False
-        return s
-
-    def has(self, url):
-        """
-        判断我们的 url 是否存在
-        """
-        if isinstance(url, str):
-            url = url.encode('utf8')
-        try:
-            # attr = self.db.get(url)
-            attr = self.db.exists(url)
-            return attr
-        except:
-            pass
-        return False
+if __name__ == '__main__':
+	url = "https://editor.csdn.net/md?not_checkout=1&articleId=1172328361"
+	urldb = UrlDB(0)
+	urldb.set_success(url)
+	print(urldb.has(url))
