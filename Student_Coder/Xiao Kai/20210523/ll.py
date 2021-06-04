17f1204e549d5b8094c4ad5773049d285f7b460b
aiyuechuang <1432803776@qq.com>
2021-06-04 10:45:48 +0800

    update

---
+++ Student_Coder/Xiao Kai/20210523/ll.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/2 3:50 下午
+# @Author  : AI悦创
+# @FileName: ll.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+print(range(10))
