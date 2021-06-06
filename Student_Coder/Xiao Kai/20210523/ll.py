1ed423d3608285fd4be15192600bbc6e63e47da7
aiyuechuang <1432803776@qq.com>
2021-06-06 14:43:02 +0800

    update

--- Student_Coder/Xiao Kai/20210523/ll.py
+++ Student_Coder/Xiao Kai/20210523/ll.py
@@ -5,4 +5,20 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
-print(range(10))
+import json
+
+class Student(object):
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+s = Student('Bob', 20, 88)
+
+def student2dict(std):
+    return {
+        'name': std.name,
+        'age': std.age,
+        'score': std.score
+    }
+print(json.dumps(s, default=student2dict))
