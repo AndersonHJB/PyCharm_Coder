71f37853674df7a7079013e83625c78b7ad9b0c7
aiyuechuang <1432803776@qq.com>
2021-06-09 19:09:36 +0800

    update

--- 网易云课堂/猿人学/section1-爬虫框架设计/pickle_learn/pickle_class.py
+++ 网易云课堂/猿人学/section1-爬虫框架设计/pickle_learn/pickle_class.py
@@ -1,34 +1,44 @@
 import pickle
-class Pickle_Study():
-	def __init__(self, waiting):
-		self.name = "Tester"
-		self.waiting = waiting
 
-	def load_cache(self, ):
-		path = self.name + '.pkl'
-		try:
-			with open(path, 'rb') as f:
-				self.waiting = pickle.load(f)
-			cc = [len(v) for k, v in self.waiting.items()]
-			print(self.waiting)
-			print('saved pool loaded! urls:', sum(cc))
-		except:
-			print("No")
-			pass
-	
-	def dump_cache(self):
-		path = self.name + '.pkl'
-		try:
-			with open(path, 'wb') as f:
-				pickle.dump(self.waiting, f)
-			print('self.waiting saved!')
-		except:
-			print("No")
-			pass
+
+class Pickle_Study():
+    """
+    存储程序运行中的内存数据
+    dump_cache：进行序列化存储到硬盘。
+    load_cache：反序列化到内存中
+    """
+    def __init__(self, waiting):
+        self.name = "Tester"
+        self.waiting = waiting
+
+    def load_cache(self, ):
+        path = self.name + '.pkl'
+        try:
+            with open(path, 'rb') as f:
+                self.waiting = pickle.load(f)
+            # cc = [len(v) for k, v in self.waiting.items()]
+            cc = [v for k, v in self.waiting.items()]
+            print("cc", cc)
+            # print("self.waiting", type(self.waiting))
+            print('saved pool loaded! urls:', len(cc))
+        except:
+            print("No")
+            pass
+
+    def dump_cache(self):
+        path = self.name + '.pkl'
+        try:
+            with open(path, 'wb') as f:
+                pickle.dump(self.waiting, f)
+            print('self.waiting saved!')
+        except:
+            print("No")
+            pass
+
 
 if __name__ == '__main__':
-	waiting = {"aiyc": "http://www.aiyc.top", "biy": "http://www.blovey.art"}
-	p = Pickle_Study(waiting)
-	d = dict(waiting)
-	print(type(d))
-	print(type(p.load_cache()))
+    waiting = {"aiyc": "http://www.aiyc.top", "biy": "http://www.blovey.art"}
+    p = Pickle_Study(waiting)
+    p.dump_cache()
+    p.load_cache()
+    # print(type(p.load_cache()))
